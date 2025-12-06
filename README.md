d02a46e1f97af656e350a389c85a89c76a6dc102
Aaron Dalton <aaron@daltons.ca>
2025-12-06 16:53:30 -0700

    Initial commit (working Thumbnails module)

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-backend
+Backend cron jobs for AbstractPlay using severless lambdas written in node.js and using DynamoDB.
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type { BasicRec } from "./BasicRec.js";
+export type { GameRec } from "./GameRec.js";
+export type { StatSummary } from "./stats/index.js";
