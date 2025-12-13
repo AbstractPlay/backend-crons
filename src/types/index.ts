673e6890672c2aea15e7c77c039bd1df73382d92
Aaron Dalton <aaron@daltons.ca>
2025-12-12 18:51:09 -0700

    Moved stats summary scripts here

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+export * from "./stats/index.js";
 export type { BasicRec } from "./BasicRec.js";
 export type { GameRec } from "./GameRec.js";
-export type { StatSummary } from "./stats/index.js";
+export type { MoveRec } from "./MoveRec.js";
+export type { Tournament } from "./Tournament.js"
+export type { OrgEvent } from "./OrgEvent.js";
+export type { OrgEventGame } from "./OrgEventGame.js";
